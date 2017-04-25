7350aade44e7e14b1fc377c90bcf608000fcf8ab
maciej.wolejszo <maciej.wolejszo@gmail.com>
2017-04-25 21:53:47 +0200

    Module 04: C# Basics; Lesson 03 - Example Hello World;

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// My first program - Again;
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Maciej :);");
+            Console.WriteLine("Welcome in C# world :);");
+            Console.ReadKey();
+        }
+    }
+}
